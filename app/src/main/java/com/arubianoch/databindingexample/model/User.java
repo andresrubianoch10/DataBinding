31777d77580af14db95e9d9fc0f9073794fdbf27
Andres Rubiano Del Chiaro <andres.rubiano.ch@gmail.com>
2019-02-01 22:24:08 -0500

    Added properties in User class; besides, Add Data tag into layout which has a reference on this new user object and their attributes

--- app/src/main/java/com/arubianoch/databindingexample/model/User.java
+++ app/src/main/java/com/arubianoch/databindingexample/model/User.java
@@ -4,4 +4,20 @@ package com.arubianoch.databindingexample.model;
  * @author Andres Rubiano Del Chiaro
  */
 public class User {
+
+    public final String firstName;
+    public final String lastName;
+
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
 }
